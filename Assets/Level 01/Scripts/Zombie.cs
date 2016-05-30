69bd8775fde7775c23f2497ac374e639b8c0916a
Kieran <github@pixelchisel.com>
2016-05-30 15:42:08 +0100

    Level 01 Added
    Level added with gun, fires bullet, chasing zombie and spawner.

---
+++ Assets/Level 01/Scripts/Zombie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+    public GameObject target;
+
+    void Start()
+    {
+        target = GameObject.Find("[CameraRig]");
+
+        if (target)
+            GetComponent<NavMeshAgent>().destination = target.transform.position;
+    }
+}
